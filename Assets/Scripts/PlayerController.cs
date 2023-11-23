6f0238c9af48460b29c3f913e09a9b8c474a4875
Leonardo Esteves <61708681+leoestevves@users.noreply.github.com>
2023-11-23 16:59:49 -0300

    Making player, ground

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    Rigidbody ballRigidbody;
+
+    float xInput;
+    float zInput;
+
+    void Awake()
     {
-        
+        ballRigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (transform.position.y < -5f)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical"); // Y seria para cima e para baixo, como um pulo
+
+        ballRigidbody.AddForce(xInput * speed, 0, zInput * speed);        
     }
 }
